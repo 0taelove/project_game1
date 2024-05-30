802ed8956528d60694a2da0d4b3245a69546e1d3
0taelove <0taelove@gmailc.com>
2024-05-30 10:41:34 +0900

    개인 프로젝트 레포지토리 이동

--- src/main/java/org/yeongtae/Application.java
+++ src/main/java/org/yeongtae/Application.java
@@ -1,7 +1,9 @@
 package org.yeongtae;
 
+import org.yeongtae.main.MainRouter;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("프로그램 시작!");
+        MainRouter.getInstance().start();
     }
 }
